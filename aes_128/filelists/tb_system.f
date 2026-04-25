66b960449e26c122ca76004f59968bfae44af7b3
sobociakk <jakub.sobota18@gmail.com>
2026-04-25 23:58:39 +0200

    aes 128 fixed

--- aes_128/filelists/tb_system.f
+++ aes_128/filelists/tb_system.f
@@ -1,12 +1,3 @@
 -f filelists/rtl.f
 
--i tb/common/utils
--i tb/common/generators
--i tb/common/drivers
--i tb/common/monitors
--i tb/common/scoreboard
--i tb/common/coverage
--i tb/system/env
--i tb/system/tests
-
 tb/unit/aes_tb.sv
--- aes_128/tb/system/env/calc_out_tx.sv
+++
@@ -1,8 +0,0 @@
-class calc_out_tx;
-    logic [3:0] anode;
-    logic [3:0] digit_val;
-
-    function void print();
-        $display("[OUT_TX] Anode: %b, Value: %0d", anode, digit_val);
-    endfunction
-endclass
